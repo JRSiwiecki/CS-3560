7e0c8f8355c0b81c60f2d1992f9fd0c1fce6910e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-03-10 00:57:42 -0800

    Add student and transcript classes

--- Assignment 2/src/Q5/Student.java
+++ Assignment 2/src/Q5/Student.java
@@ -3,8 +3,15 @@ public class Student
 	private String name;
 	private Transcript transcript;
 
+	public Student(String name)
+	{
+		this.name = name;
+		transcript = null;
+	}
+	
 	public Student(String name, Transcript transcript)
 	{
-		
+		this.name = name;
+		this.transcript = transcript;
 	}
 }
