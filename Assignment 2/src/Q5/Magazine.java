10c1d7c22676e13d1e95690963f9b9fd10825ee3
JRSiwiecki <JRSiwiecki@gmail.com>
2023-03-10 01:21:37 -0800

    Add SaleableItem, Magazine, and Ticket classes

---
+++ Assignment 2/src/Q5/Magazine.java
@@ -0,0 +1,8 @@
+public class Magazine implements SaleableItem
+{
+    @Override
+    public void sellCopy()
+    {
+        System.out.println("Selling a magazine.");
+    }
+}
---
+++ Assignment 2/src/Q5/Ticket.java
@@ -0,0 +1,8 @@
+public class Ticket implements SaleableItem
+{
+    @Override
+    public void sellCopy()
+    {
+        System.out.println("Selling a ticket.");
+    }
+}
