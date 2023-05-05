ff69a8496d634a6cc40c440b02561044183038f5
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-04 20:04:12 -0700

    Initial assignment 4

---
+++ Assignment4/src/Order.java
@@ -0,0 +1,56 @@
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="order")
+public class Order 
+{
+	private int number;
+	private Date date;
+	private String item;
+	private double price;
+	
+	private Customer customer;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	
+}
