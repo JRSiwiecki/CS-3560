ddd2a1dfd14b077c0fa3deb05fb788870ba9fa52
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-05 22:52:31 -0700

    Mostly finish Customer GUI

--- Assignment4/src/CustomerGUI.java
+++ Assignment4/src/CustomerGUI.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class CustomerGUI
 {
@@ -15,12 +14,14 @@ public class CustomerGUI
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// Create components and add them to the JFrame
-		JPanel panel = new JPanel(new FlowLayout());
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
         
         // Create labels
         JLabel nameLabel = new JLabel("Name");
         JLabel phoneLabel = new JLabel("Phone");
         JLabel emailLabel = new JLabel("Email");
+        JLabel addressLabel = new JLabel("Address");
         JLabel streetLabel = new JLabel("Street");
         JLabel cityLabel = new JLabel("City");
         JLabel stateLabel = new JLabel("State");
@@ -42,36 +43,58 @@ public class CustomerGUI
         JButton deleteButton = new JButton("Delete");
 
         // Add name components
+        nameLabel.setBounds(25, 25, 100, 25);
+        nameField.setBounds(25, 50, 925, 25);
         panel.add(nameLabel);
         panel.add(nameField);
         
         // Add phone components
+        phoneLabel.setBounds(25, 100, 100, 25);
+        phoneField.setBounds(25, 125, 300, 25);
         panel.add(phoneLabel);
         panel.add(phoneField);
         
         // Add email components
+        emailLabel.setBounds(350, 100, 100, 25);
+        emailField.setBounds(350, 125, 600, 25);
         panel.add(emailLabel);
         panel.add(emailField);
         
+        // Add address label
+        addressLabel.setBounds(25, 200, 100, 25);
+        panel.add(addressLabel);
+        
         // Add street components
+        streetLabel.setBounds(50, 250, 100, 25);
+        streetField.setBounds(50, 275, 300, 25);
         panel.add(streetLabel);
         panel.add(streetField);
         
         // Add city components
+        cityLabel.setBounds(450, 250, 100, 25);
+        cityField.setBounds(450, 275, 300, 25);
         panel.add(cityLabel);
         panel.add(cityField);
         
         // Add state components
+        stateLabel.setBounds(50, 350, 100, 25);
+        stateField.setBounds(50, 375, 300, 25);
         panel.add(stateLabel);
         panel.add(stateField);
         
         // Add zip code components
+        zipCodeLabel.setBounds(450, 350, 100, 25);
+        zipCodeField.setBounds(450, 375, 300, 25);
         panel.add(zipCodeLabel);
         panel.add(zipCodeField);
         
         
         
         // Add buttons to panel
+        searchButton.setBounds(500, 500, 75, 30);
+        addButton.setBounds(600, 500, 75, 30);
+        updateButton.setBounds(700, 500, 75, 30);
+        deleteButton.setBounds(800, 500, 75, 30);
         panel.add(searchButton);
         panel.add(addButton);
         panel.add(updateButton);
@@ -86,19 +109,19 @@ public class CustomerGUI
         });
         
      // Add event listeners and handlers
-        searchButton.addActionListener(e -> {
+        addButton.addActionListener(e -> {
             String name = nameField.getText();
             JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
         });
         
      // Add event listeners and handlers
-        searchButton.addActionListener(e -> {
+        updateButton.addActionListener(e -> {
             String name = nameField.getText();
             JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
         });
         
      // Add event listeners and handlers
-        searchButton.addActionListener(e -> {
+        deleteButton.addActionListener(e -> {
             String name = nameField.getText();
             JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
         });
